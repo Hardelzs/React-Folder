4634de0ecda07c1740a2dc12f7e85c3b55af6f70
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-19 17:15:13 +0100

    styled the app.css

--- calculator1/src/App.js
+++ calculator1/src/App.js
@@ -10,11 +10,30 @@ function App() {
         <div className='main2'></div>
         <div className='main3'></div>
         <div className='main4'></div>
-        <div className='main5'>5</div>
+        <div className='main5'></div>
       </div>
 
       <div className='mart'>
-
+        <fieldset>
+          <form>
+            <div>
+              <label>Firstname*</label>
+              <input className="" placeholder="Name"/>  
+            </div>
+            <div>
+              <label>Last Name*</label>
+              <input className="" placeholder="Name"/>  
+            </div>
+            <div>
+              <label>Enter Email*</label>
+              <input className="" placeholder="Name"/>  
+            </div>
+            <div>
+              <label>Contact*</label>
+              <input className="" placeholder="Name"/>  
+            </div>
+          </form>
+        </fieldset>
       </div>
 
     </div>

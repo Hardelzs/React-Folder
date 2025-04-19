6dc5fe7ef47b60e897e484145fb41ad420e87a45
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-19 10:11:35 +0100

    include the animation

--- calculator1/src/App.js
+++ calculator1/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='play'>
+        <div className='main'></div>
+        <div className='main1'></div>
+        <div className='main2'></div>
+        <div className='main3'></div>
+        <div className='main4'></div>
+        <div className='main5'></div>
+      </div>
+
+      <div className='mart'>
+        
+      </div>
+
     </div>
   );
 }

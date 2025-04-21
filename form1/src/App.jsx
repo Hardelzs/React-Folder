9d1a8f7f6b7c231850df12696d929cfef29bd3fb
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-21 22:38:44 +0100

    re writting and testing the code

--- form1/src/App.jsx
+++ form1/src/App.jsx
@@ -1,35 +1,9 @@
-import React, { useState } from 'react'
+
 import "./App.css";
 
 const App = () => {
-  const [firstname, setFirstName] = useState("")
-  const [secondname, setsecondname] = useState("")
-
-  const handleSubmit = (e) => {
-    e.preventDefault;
-    const data = {
-      firstname,
-      secondname
-    }
-    console.log("form:", data);
-    
-  }
-
-  const handleReset = () => {
-    setFirstName("")
-    setsecondname("")
-  }
   return (
     <div className='flex'>
-      <label htmlFor="">First name</label>
-      <input type="text" name="" id="" value={firstname} onChange={(e) => setFirstName(e.target.value)} />
-
-      <label htmlFor="">First name</label>
-      <input type="text" name="" id="" value={secondname} onChange={(e) => setsecondname(e.target.value)} />
-
-
-      <button onClick={(e) => handleSubmit(e)}>Submit</button>
-      <button onClick={(e) => handleReset(e)}>Reset</button>
     </div>
   )
 }

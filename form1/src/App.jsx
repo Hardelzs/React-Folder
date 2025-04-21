20f4f5d06ae7c399501fb9fdb29ca022936040e8
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-21 22:47:28 +0100

    done with the firstname

--- form1/src/App.jsx
+++ form1/src/App.jsx
@@ -1,9 +1,32 @@
 
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [firstname, setFirstname] = useState("")
+
+  const handlesubmit = (e) => {
+    e.preventDefault;
+    const data = {
+      firstname
+    }
+    console.log("Form", data);
+    
+  }
+
+  const handlereset = () => {
+    setFirstname("")
+  }
   return (
     <div className='flex'>
+      <label htmlFor="">First Name</label>
+      <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+
+      <label htmlFor="">Second Name </label>
+      <input type="text"   />
+
+      <button type="submit" onClick={(e) => handlesubmit(e)}>Submit</button>
+      <button type="submit" onClick={(e) => handlereset(e)}>Reset</button>
     </div>
   )
 }

6189a71d8dbe1b38e2da9da81ee5900cba98d641
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-26 22:55:15 +0100

    Enhance NavBar component with styling and structure; add CSS for main layout

--- form1/src/Components/NavBar.jsx
+++ form1/src/Components/NavBar.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../CSS/Navbar.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="main">
+      <div className="movie">
+        <Link to={"/"}>HdrMovie</Link>
+      </div>
+
+      <div className="otherMov">
         <Link to={"/"}>
-            <h1>Home</h1>
+          <h1>Home</h1>
         </Link>
         <Link to={"/favourite"}>
-            <h1>Favourite</h1>
-            </Link>
+          <h1>Favourite</h1>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

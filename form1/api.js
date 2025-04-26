42142c4d3d405247e2328bf19127567cb45fa77a
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-26 08:34:25 +0100

    Add API functions to fetch popular movies and search movies

---
+++ form1/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "28acb4e35f0b3cbc2284b1ebed9f0c8c"
+const API_URL = "https://api.themoviedb.org/3"
+
+export const fetchPopularMovies = async () => {
+    const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+
+export const searchMovies = async () => {
+    const response = await fetch(`${API_URL}/search/movie?api_key=${API_KEY}$query={encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}

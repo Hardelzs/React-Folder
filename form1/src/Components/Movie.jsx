35b6a4bc039563a13e239df1e95360ae25e837c1
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-27 19:07:15 +0100

    Refactor Movie component to manage button click state and update rendering logic

--- form1/src/Components/Movie.jsx
+++ form1/src/Components/Movie.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Movie = ({movie}) => {
+    const [buttonClick, setButtonClick] = useState(false)
     const handleClick =() => {
-        alert("Clicked")
+        console.log(buttonClick);
+        
     }
+    useEffect(() => {
+        setButtonClick("")
+    })
   return (
     <div className='movie-card'>
         <div className='movie-card-image'>
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={Movie.title} />
             <div>
+                {buttonClick ? "c": "b"}
                 <button className='movie-overlay' onClick={handleClick}>💗</button>
             </div>
         </div>

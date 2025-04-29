f0fc0025826ac56fb8d29199317fa43c17227797
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-29 21:22:06 +0100

    Refactor Favourite component to utilize MovieContext for managing favorites and update rendering logic

--- form1/src/Pages/Favourite.jsx
+++ form1/src/Pages/Favourite.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useMovieContext } from "../Contexts/MovieContext";
+import Movie from "../Components/Movie";
 
 const Favourite = () => {
+  const { favorites } = useMovieContext();
 
+  if (favorites) {
+    return (
+      <div>
+        <h2>Your favorite</h2>
+        <div className="movie-grid">
+          {favorites.map((movie) => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Favourite</h1>
@@ -9,7 +24,7 @@ const Favourite = () => {
       <p>Here you can see your favourite items</p>
       <p>Click on the heart icon to add an item to your favourites</p>
     </div>
-  )
-}
+  );
+};
 
-export default Favourite
+export default Favourite;

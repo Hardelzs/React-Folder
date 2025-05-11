3581e844f393c715f53794df07f376e413ac851a
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-05-11 10:42:18 +0100

    Fix script source in index.html and refactor main.jsx for consistency

--- todoApp/src/main.jsx
+++ todoApp/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
-import ReactDom from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 
-ReactDom.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 )

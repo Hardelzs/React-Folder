11d8155bf176a0fdbc34ed1a2700463aa94b55fd
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-05-11 11:12:58 +0100

    Refactor: clean up Todo and TodoItem components, update index.html title

--- todoApp/src/Component/Todo.jsx
+++ todoApp/src/Component/Todo.jsx
@@ -1,38 +1,50 @@
 import { useEffect, useRef, useState } from "react";
+import TodoItem from './TodoItem'; // Import TodoItem
 
-
-let count = 0
+let count = 0;
 const Todo = () => {
-
   const [todos, setTodos] = useState([]);
   const inputRef = useRef(null);
 
-  const add = () =>{
-    setTodos([...todos,{no:count++,text:inputRef.current.value,display:''}]);
-    inputRef.current.value = ""
-  }
+  const add = () => {
+    setTodos([...todos, { no: count++, text: inputRef.current.value, display: '' }]);
+    inputRef.current.value = "";
+  };
 
   useEffect(() => {
     console.log(todos);
-    
-  },[todos])
+  }, [todos]);
 
   return (
-    <div className="flex  items-center justify-center" >
-      <div className="mt-20 rounded-md bg-white flex-col p-4 h-auto w-1/2"
-      >
-      <div className="w-1/2 m-2 text-violet-950 font-semibold size-9 font-36">To-Do List</div>
-      <div className="flex justify-center align-center">
-        <input ref={inputRef} type="text" placeholder="Add your task" className="rounded-full bg-[#edeef0] w-[576px] h-[50px] size-5 p-4 outline-none"/>
-        <div onClick={()=>{add()}} className="text-center text-white size-6 font-semibold cursor-pointer rounded-full bg-purple-900 w-[187px] h-[50px] -ml-12">ADD</div>
-      </div>
+    <div className="flex items-center justify-center">
+      <div className="mt-20 rounded-md bg-white flex-col p-4 h-auto w-1/2">
+        <div className="w-1/2 m-2 text-violet-950 font-semibold size-9 font-36">To-Do List</div>
+        <div className="flex justify-center align-center">
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Add your task"
+            className="rounded-full bg-[#edeef0] w-[576px] h-[50px] size-5 p-4 outline-none"
+          />
+          <div
+            onClick={() => {
+              add();
+            }}
+            className="text-center text-white size-6 font-semibold cursor-pointer rounded-full bg-purple-900 w-[187px] h-[50px] -ml-12"
+          >
+            ADD
+          </div>
+        </div>
 
-      <div>
-        {todos.map((item,index) => {
-          return <div><TodoItem key={index}no={item.no} display={item.display} text={item.text}></TodoItem></div>
-        })}
-      </div>
-
+        <div>
+          {todos.map((item, index) => {
+            return (
+              <div key={index}>
+                <TodoItem no={item.no} display={item.display} text={item.text} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- todoApp/src/Component/TodoItem.jsx
+++ todoApp/src/Component/TodoItem.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import tick from './assets/tick.png'
-import notCheck from './assets/notCheck.png'
-import cross from './assets/cross.png'
+import React from 'react';
+import tick from '../assets/tick.png';
+import notCheck from '../assets/notCheck.png';
+import cross from '../assets/cross.png';
 
-const TodoItem = (no,display,text) => {
+const TodoItem = ({ no, display, text }) => { // Correctly destructure props
   return (
-    <div >
-        <div>
-            <img src={notCheck} alt="" />
-            <img src={tick} alt="" />
-            <div>{text}</div>
-        </div>
-        <img src={cross} alt="" />
+    <div>
+      <div>
+        <img src={notCheck} alt="" />
+        <img src={tick} alt="" />
+        <div>{text}</div>
+      </div>
+      <img src={cross} alt="" />
     </div>
-  )
-}
+  );
+};
 
-
-export default TodoItem
+export default TodoItem;
